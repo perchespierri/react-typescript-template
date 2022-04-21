1d56704312ff562bd74d683f67eb2de529482e53
perchespierri <perchespierri@gmail.com>
2022-04-21 13:28:04 -0300

    Add some silly code to remind myself of how to type a few things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import CounterFunction from "./interfaces";
+
+interface Person {
+  name: string,
+  age: number
+}
+
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const showSum = (a: number, b: number): string => `${a + b}`;
+
+  const showPerson = ({ name, age }: Person): string => {
+    return `NAME - ${name}, AGE - ${age}`;
+  }
+
+  const addToCounter: CounterFunction = () => setCounter(counter + 1);
+
+  const subtractFromCounter: CounterFunction = () => {
+    setCounter(counter - 1);
+    return "hey";
+  };
+
   return (
     <>
-      APP COMPONENT
+      <h1>Sum of 2 and 3: {showSum(2, 3)}</h1>
+
+      <h2>Person: {showPerson({ age: 28, name: "Rafa" })}</h2>
+
+      <button onClick={addToCounter}>ADD</button>
+      <h3>{counter}</h3>
+      <button onClick={subtractFromCounter}>SUBTRACT</button>
+
     </>
   );
 }
